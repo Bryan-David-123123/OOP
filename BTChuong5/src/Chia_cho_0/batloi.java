c5f889d401f4392f1f765f7ea4f9dfa4448338ea
KhanhNV <102637873+Bryan-David-123123@users.noreply.github.com>
2024-11-13 17:08:51 +0700

    Delete BTChuong5 directory

--- BTChuong5/src/Chia_cho_0/batloi.java
+++
@@ -1,7 +0,0 @@
-package Chia_cho_0;
-
-public class batloi extends Exception{
-	public batloi(String loi) {
-		super(loi);
-	}
-}
--- BTChuong5/src/Ngoai_pham_vi/OutOfRangeException.java
+++
@@ -1,7 +0,0 @@
-package Ngoai_pham_vi;
-
-public class OutOfRangeException extends Exception{
-	public OutOfRangeException(String loi) {
-		super(loi);
-	}
-}
