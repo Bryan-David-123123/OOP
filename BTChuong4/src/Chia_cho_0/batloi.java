a0f7e27e5b118ad55f04165560891a498c79feb6
KhanhNV <102637873+Bryan-David-123123@users.noreply.github.com>
2024-11-13 17:09:09 +0700

    Add files via upload

---
+++ BTChuong4/src/Chia_cho_0/batloi.java
@@ -0,0 +1,7 @@
+package Chia_cho_0;
+
+public class batloi extends Exception{
+	public batloi(String loi) {
+		super(loi);
+	}
+}
---
+++ BTChuong4/src/Ngoai_pham_vi/OutOfRangeException.java
@@ -0,0 +1,7 @@
+package Ngoai_pham_vi;
+
+public class OutOfRangeException extends Exception{
+	public OutOfRangeException(String loi) {
+		super(loi);
+	}
+}
